b5da7be4a2990788f74b9b93ca36053aac59682c
ywatanabe <yu.w.tennis@gmail.com>
2020-11-05 09:05:10 +0900

    Changed file name for main.py same as main function for python.

---
+++ src/__main__.py
@@ -0,0 +1,29 @@
+
+import sys
+from importlib import import_module
+from pkgs.utils.args import *
+from pkgs.config import *
+
+def main(club):
+
+    if club not in MODULES:
+        raise(ModuleNotFoundError())
+
+    # Load base class
+    c = getattr(import_module(MODULES[club]['module']), MODULES[club]['class'])
+
+    # Instantiate
+    t = c()
+
+    res = t.run()
+
+    # Display
+    t.display(res)
+
+    # Notify
+    t.notify_to_console(res)
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    main(args.tennis_club)

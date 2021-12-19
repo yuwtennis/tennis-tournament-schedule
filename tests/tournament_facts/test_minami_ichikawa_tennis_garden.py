8ee0b1b4436e8abd04f615fbcf073318796aecd9
Yu Watanabe <y-watanabe@creationline.com>
2021-12-19 18:25:41 +0900

    Fix wrong test name.

--- tests/tournament_facts/test_minami_ichikawa_tennis_garden.py
+++ tests/tournament_facts/test_minami_ichikawa_tennis_garden.py
@@ -4,31 +4,31 @@ from tournaments.tournament_facts_spec import Venue, SemiOpenSingles, AdvanceDou
 def test_venue_success():
     result = Venue().dump({})
 
-    assert result['venue_name_jpn'] == '南市川テニスガーデン'
+    assert result['venue_name'] == '南市川テニスガーデン'
 
 
 def test_semi_open_singles_success():
     result = SemiOpenSingles().dump({})
 
-    assert result['venue_name_jpn'] == '南市川テニスガーデン'
-    assert result['match_type_jpn'] == '男子シングルス'
-    assert result['level_jpn'] == 'セミオープン'
+    assert result['venue_name'] == '南市川テニスガーデン'
+    assert result['match_type'] == '男子シングルス'
+    assert result['level'] == 'セミオープン'
     assert result['url'] == 'https://minamiichikawa.jp/tennis_tournament/pg407.html'
 
 
 def test_advance_doubles_success():
     result = AdvanceDoubles().dump({})
 
-    assert result['venue_name_jpn'] == '南市川テニスガーデン'
-    assert result['match_type_jpn'] == '男子ダブルス'
-    assert result['level_jpn'] == 'アドバンス'
+    assert result['venue_name'] == '南市川テニスガーデン'
+    assert result['match_type'] == '男子ダブルス'
+    assert result['level'] == 'アドバンス'
     assert result['url'] == 'https://minamiichikawa.jp/tennis_tournament/pg408.html'
 
 
 def test_mix_doubles_success():
     result = AdvanceDoubles().dump({})
 
-    assert result['venue_name_jpn'] == '南市川テニスガーデン'
-    assert result['match_type_jpn'] == '混合ダブルス'
-    assert result['level_jpn'] == 'アドバンス'
+    assert result['venue_name'] == '南市川テニスガーデン'
+    assert result['match_type'] == '混合ダブルス'
+    assert result['level'] == 'アドバンス'
     assert result['url'] == 'https://minamiichikawa.jp/tennis_tournament/pg408.html'

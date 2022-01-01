d5d9a2310c6c77d9b9c6e595cb8b1f90d65d1de3
Yu Watanabe <yu.w.tennis@gmail.com>
2022-01-01 16:34:53 +0900

    WIP: Google calendar registration.

--- tournaments/calendar.py
+++ tournaments/calendar.py
@@ -1,11 +1,53 @@
 """ Stateless Service """
-from tournaments import ScheduleEntity
+from typing import List
+from datetime import timedelta
+from hashlib import sha256
+import logging
+from tournaments.helper import is_event_registered
+
+LOGGER = logging.getLogger(__name__)
 
 
 class CalendarService:
 
     @staticmethod
-    def register_google_calendar(self, schedule: ScheduleEntity):
-        """ Register google calendar """
+    def register_google_calendar(self, service, calendar_id, schedules: List):
+        """ Register events to specified google calendar """
 
-        raise NotImplemented()
+        for schedule in schedules:
+
+            inst = schedule.dump({})
+
+            event = {
+                'summary': f'{inst["match_type"]} {inst["venue_name"]}',
+                'start': {
+                    'dateTime': inst['match_date'].isoformat(),
+                    'timeZone': inst['match_date'].zone,
+                },
+                'end': {
+                    'dateTime':
+                        (inst['match_date'] + timedelta(hours=5)).isoformat(),
+                    'timeZone': inst['match_date'].zone,
+                }
+            }
+
+            event['id'] = sha256(f'{event["summary"]}').hexdigest()
+
+            try:
+                if is_event_registered(event['id']):
+                    service.events()\
+                        .update(
+                            calendarId=calendar_id,
+                            eventId=event['id'],
+                            body=event).execute()
+                else:
+                    service.events()\
+                        .insert(
+                            calendarId=calendar_id,
+                            body=event).execute()
+
+            except Exception:
+                LOGGER.error('Failed to register event. %s', event['summary'])
+
+            LOGGER.info('Completed event registering.')
+
--- tournaments/helper.py
+++ tournaments/helper.py
@@ -9,4 +9,26 @@ def parse_jpn_date(jpn_date_str: str, starts_at: str) -> datetime:
     time = starts_at.split(':')
     jst = timezone('Asia/Tokyo')
 
-    return jst.localize(match.group(1), match.group(2), 1, int(time[0]), int(time[1]))
+    return jst.localize(
+        match.group(1), match.group(2), 1, int(time[0]), int(time[1]))
+
+
+def is_event_registered(service, calendar_id, event_id):
+    """
+    Return Google Calendar Id from summary.
+
+    Parameters
+    ----------
+    service
+    calendar_id
+    event_id
+
+    Returns
+    -------
+    bool:
+        Confirmation result of event id
+
+    """
+    events = service.events().list(calendarId=calendar_id, q=f"id={event_id}")
+
+    return True if len(events['items']) > 0 else False
